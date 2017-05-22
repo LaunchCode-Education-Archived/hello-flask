9a7a311c11e7a6da59e45edfa9d074e0e710667e
Chris Bay <chris@chrisbay.net>
2017-05-22 08:03:58 -0500

    End of redirect lesson

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from flask import Flask, request
+from flask import Flask, request, redirect
 
 app = Flask(__name__)
 app.config['DEBUG'] = True
@@ -85,12 +85,18 @@ def validate_time():
             minutes = ''
 
     if not minutes_error and not hours_error:
-        return "Success!"
+        time = str(hours) + ':' + str(minutes)
+        return redirect('/valid-time?time={0}'.format(time))
     else:
         return time_form.format(hours_error=hours_error,
             minutes_error=minutes_error,
             hours=hours,
             minutes=minutes)
 
+
+@app.route('/valid-time')
+def valid_time():
+    time = request.args.get('time')
+    return '<h1>You submitted {0}. Thanks for submitting a valid time!</h1>'.format(time)
 
 app.run()
