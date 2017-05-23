4b4858d1b18d13dbf70de8e599ea45f7241d250b
Chris Bay <chris@chrisbay.net>
2017-05-22 20:06:43 -0500

    Refactor time validation code to use templates

--- main.py
+++ main.py
@@ -21,28 +21,10 @@ def hello():
     return template.render(name=first_name)
 
 
-time_form = """
-    <style>
-        .error {{ color: red; }}
-    </style>
-    <h1>Validate Time</h1>
-    <form method='POST'>
-        <label>Hours (24-hour format)
-            <input name="hours" type="text" value='{hours}' />
-        </label>
-        <p class="error">{hours_error}</p>
-        <label>Minutes
-            <input name="minutes" type="text" value='{minutes}' />
-        </label>
-        <p class="error">{minutes_error}</p>
-        <input type="submit" value="Validate" />
-    </form>
-    """
-
 @app.route('/validate-time')
 def display_time_form():
-    return time_form.format(hours='', hours_error='',
-        minutes='', minutes_error='')
+    template = jinja_env.get_template('time_form.html')
+    return template.render()
 
 
 def is_integer(num):
@@ -84,7 +66,8 @@ def validate_time():
         time = str(hours) + ':' + str(minutes)
         return redirect('/valid-time?time={0}'.format(time))
     else:
-        return time_form.format(hours_error=hours_error,
+        template = jinja_env.get_template('time_form.html')
+        return template.render(hours_error=hours_error,
             minutes_error=minutes_error,
             hours=hours,
             minutes=minutes)
